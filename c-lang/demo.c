e9f07d63d98bfe845f6a618006d533dce3623701
seanschen <seanschen@tencent.com>
2020-03-30 18:57:16 +0800

    提交并同步代码: 2020-03-30 18:57:16

--- c-lang/demo.c
+++ c-lang/demo.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("test\n");
-}
+    int a;
+    printf("请输入一个不大于1000的数字: \n");
+    scanf("%d", &a);
 
-// 编译提示
-// gcc -o ~/demo demo.c
+    if (a > 1000 || a < 0) {
+        printf("输入错误\n");
+    }
+
+    if (a > 99 && a < 1000) {
+        printf("百位数字是: %d\n", a/100%10);
+        printf("十位数字是: %d\n", a/10%10);
+        printf("个位数字是: %d\n", a/1%10);
+    } else if (a > 9 && a < 100) {
+        printf("十位数字是: %d\n", a/10%10);
+        printf("个位数字是: %d\n", a/1%10);
+    } else {
+        printf("个位数字是: %d\n", a/1%10);
+    }
+
+    // 等待输入，防止程序退出
+    scanf("%d", &a);
+}
---
+++ c-lang/demo1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main() {
+    int num, total = 0;
+    // 百，十，个 
+    int c, d, e; 
+
+    printf("请输入一个不多于3位的正整数：");
+    scanf("%d", &num);
+
+    c = (num/100)%10;//取百位数值 
+    d = (num/10)%10;//取十位数值
+    e = num%10;//取个位数值
+
+    if (c!= 0) {
+        total++;
+    }
+    if (d!= 0) {
+        total++;
+    }
+    if (e!= 0) {
+        total++;
+    }
+
+    printf("\n你输入的 %d 是 %d 位数\n", num, total);
+
+    switch(total) {
+        case 1:
+            printf("个位: %d\n", e);
+            break;
+        case 2:
+            printf("个位: %d\n", e);
+            printf("十位: %d\n", d);
+            break;
+        case 3:
+            printf("个位: %d\n", e);
+            printf("十位: %d\n", d);
+            printf("百位: %d\n", c);
+            break;
+    }
+
+    return 0;
+}
